b4afcee781be4f75667ec428c829c7c1547a9d8a
James Harding <jamesvitaly1993@gmail.com>
2018-11-07 18:42:20 +0000

    Created a better animation for the icon button pulse

--- src/components/_common/IconButton.jsx
+++ src/components/_common/IconButton.jsx
@@ -4,15 +4,36 @@ import theme from '../../theme';
 
 const pulse = keyframes`
 0% {
-  transform: scale(0.9);
-  box-shadow: 0 0 0 10px rgba(7, 102, 208, 0.2);
+  box-shadow: 0 0 0 1px rgba(7, 102, 208, 0.9);
 }
-70% {
-  transform: scale(1);
-  box-shadow: 0 0 0 5px rgba(7, 102, 208, 0);
+5% {
+  box-shadow: 0 0 0 2px rgba(7, 102, 208, 0.8);
 }
-  100% {
-  transform: scale(0.9);
+10% {
+  box-shadow: 0 0 0 3px rgba(7, 102, 208, 0.7);
+}
+15% {
+  box-shadow: 0 0 0 4px rgba(7, 102, 208, 0.6);
+}
+20% {
+  box-shadow: 0 0 0 5px rgba(7, 102, 208, 0.5);
+}
+25% {
+  box-shadow: 0 0 0 6px rgba(7, 102, 208, 0.4);
+}
+30% {
+  box-shadow: 0 0 0 7px rgba(7, 102, 208, 0.3);
+}
+35% {
+  box-shadow: 0 0 0 8px rgba(7, 102, 208, 0.2);
+}
+40% {
+  box-shadow: 0 0 0 9px rgba(7, 102, 208, 0.1);
+}
+50% {
+  box-shadow: 0 0 0 10px rgba(7, 102, 208, 0.05);
+}
+100% {
   box-shadow: 0 0 0 0 rgba(7, 102, 208, 0));
 }
 `;
